846c82250127d0596312da6ed0b880be8c216b59
TGYT <41660086+TGYT@users.noreply.github.com>
2018-11-08 14:30:16 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Tops ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Tops | thelp`,"http://twitch.tv/Death Shop")
+client.user.setGame(`Tops | thelp`)
 client.user.setStatus("online")
 });
 
