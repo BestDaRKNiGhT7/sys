d7f0ce7a3a5867c7e2ab85cd30d5e52613544fdd
TGYT <41660086+TGYT@users.noreply.github.com>
2018-11-09 12:36:26 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,6 @@
 const Discord = require('discord.js');  
 const db = require('quick.db');  
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : Tops`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : Tops ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Tops | thelp`)
+client.user.setActivity(`Tops | -help`)
 client.user.setStatus("online")
 });
 
