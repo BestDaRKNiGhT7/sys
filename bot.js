406c9db40294b873368cc5d0d2604fb827877688
TGYT <41660086+TGYT@users.noreply.github.com>
2018-12-03 20:57:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,6 +32,59 @@ const sql = require("sqlite");
 ,spee={};
 
  
+client.on('message',async message => {
+    if(message.content.startsWith(code + "comp")) {
+  if(!message.channel.guild) return message.reply('This Command For Servers Only !');
+    let jscodes = message.guild.channels.find(`name`, "تُصِوِيّتُٱتُ");
+    if(!jscodes) return message.channel.send(":x:لم اجد الروم الخاص بنشر التصويتات");
+      let filter = m => m.author.id === message.author.id;
+      let thisMessage;
+      let thisFalse;
+      message.channel.send(':pencil: **| من فضلك اكتب الكود الأن... :pencil2: **').then(msg => {
+ 
+      message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 90000,
+        errors: ['time']
+      })
+      .then(collected => {
+        collected.first().delete();
+        thisMessage = collected.first().content;
+                boi2 = collected.first().content;
+        msg.edit(':shield: **| [ هل انت متأكد من نشر الكود؟ | [ نعم ] او [ لا**');
+   message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
+          max: 1,
+          time: 90000,
+          errors: ['time']
+        })
+        .then(collected => {
+          if(collected.first().content === 'لا') {
+            msg.delete();
+            message.delete();
+            thisFalse = false;
+          }
+          if(collected.first().content === 'نعم') {
+            if(thisFalse === false) return;
+            msg.edit(':dove: **| Done :white_check_mark:, تم بنجاح نشر كودك في روم الاكواد**');
+            collected.first().delete();
+            jscodes.send(`@مسابقة
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+**Slayers © :arrow_down:**            
+\*\*js
+${thisMessage}\*\*
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+**تم النشر بواسطة**: ${message.author}
+          }
+        }
+    );
+});
+      });
+    }
+      );
+    });
+}
+);
+      })}});
 
 const superagent = require("superagent")
 client.on('message' , async (message) => {
