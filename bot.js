bd0bd54f39ac4583fd6e22f632ae5f83fb9b0a74
TGYT <41660086+TGYT@users.noreply.github.com>
2018-12-03 22:42:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ var prefix = "-";
 if(message.channel.type === "dm") return;
 if(message.author.bot) return;
   if(!sWlc[message.guild.id]) sWlc[message.guild.id] = {
-    channel: "✃𝐒𝐥𝐚𝐲𝐞𝐫𝐬ツ"
+    channel: "welcome"
 }
 const channel = sWlc[message.guild.id].channel
   if (message.content.startsWith(prefix + "setwelcomer")) {
