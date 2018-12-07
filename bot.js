029655872a0b3ccccb45baf757f6b2c7b4a88cd5
TGYT <41660086+TGYT@users.noreply.github.com>
2018-12-07 20:37:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,6 +33,125 @@ const sql = require("sqlite");
 
 
 
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.toLowerCase().startsWith(prefix + `ticket`)) {
+    const embed = new Discord.RichEmbed()
+    .setTitle(`:mailbox_with_mail: Vulnix Help`)
+    .setColor(0xCF40FA)
+    .setDescription(`Hello! I'm Vulnix, the Discord bot for super cool support ticket stuff and more! Here are my commands:`)
+    .addField(`Tickets`, `[${prefix}new]() > Opens up a new ticket and tags the Support Team\n[${prefix}close]() > Closes a ticket that has been resolved or been opened by accident`)
+    .addField(`Other`, `[${prefix}help]() > Shows you this help menu your reading\n[${prefix}ping]() > Pings the bot to see how long it takes to react\n[${prefix}about]() > Tells you all about Vulnix`)
+    message.channel.send({ embed: embed });
+  }
+
+  if (message.content.toLowerCase().startsWith(prefix + `ping`)) {
+    message.channel.send(`Hoold on!`).then(m => {
+    m.edit(`:ping_pong: Wew, made it over the ~waves~ ! **Pong!**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
+    });
+}
+
+if (message.content.toLowerCase().startsWith(prefix + `new`)) {
+    const reason = message.content.split(" ").slice(1).join(" ");
+    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
+    message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+        let role = message.guild.roles.find("name", "Support Team");
+        let role2 = message.guild.roles.find("name", "@everyone");
+        c.overwritePermissions(role, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+        c.overwritePermissions(role2, {
+            SEND_MESSAGES: false,
+            READ_MESSAGES: false
+        });
+        c.overwritePermissions(message.author, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+        message.channel.send(`:white_check_mark: مرحباًً بك #${c.name}.`);
+        const embed = new Discord.RichEmbed()
+        .setColor(0xCF40FA)
+        .addField(`Hey ${message.author.username}!`, `من فضلك اكتب سبب انشاء التيكيت.`)
+        .setTimestamp();
+        c.send({ embed: embed });
+    }).catch(console.error);
+}
+if (message.content.toLowerCase().startsWith(prefix + `close`)) {
+    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+
+    message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`-confirm\`. This will time out in 10 seconds and be cancelled.`)
+    .then((m) => {
+      message.channel.awaitMessages(response => response.content === '-confirm', {
+        max: 1,
+        time: 10000,
+        errors: ['time'],
+      })
+      .then((collected) => {
+          message.channel.delete();
+        })
+        .catch(() => {
+          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+              m2.delete();
+          }, 3000);
+        });
+    });
+}
+
+});
+
+
+
+const gulp = require('gulp');
+const gulp_ts = require('gulp-typescript');
+const gulp_tslint = require('gulp-tslint');
+const gulp_sourcemaps = require('gulp-sourcemaps');
+const tslint = require('tslint');
+const del = require('del');
+const path = require('path');
+
+const project = gulp_ts.createProject('tsconfig.json');
+const linter = tslint.Linter.createProgram('tsconfig.json');
+
+gulp.task('default', ['build']);
+
+gulp.task('lint', () => {
+	gulp.src('./src/**/*.ts')
+		.pipe(gulp_tslint({
+			configuration: 'tslint.json',
+			formatter: 'prose',
+			program: linter
+		}))
+		.pipe(gulp_tslint.report());
+})
+
+gulp.task('build', () => {
+	del.sync(['./bin/**/*.*']);
+	const tsCompile = gulp.src('./src/**/*.ts')
+		.pipe(gulp_sourcemaps.init())
+		.pipe(project());
+
+	tsCompile.pipe(gulp.dest('bin/'));
+
+	gulp.src('./src/**/*.js')
+		.pipe(gulp.dest('bin/'));
+
+	gulp.src('./src/**/*.json')
+		.pipe(gulp.dest('bin/'));
+
+	gulp.src('./src/**/*.lang')
+		.pipe(gulp.dest('bin/'));
+
+	return tsCompile.js
+		.pipe(gulp_sourcemaps.write({
+			sourceRoot: file => path.relative(path.join(file.cwd, file.path), file.base)
+		}))
+		.pipe(gulp.dest('bin/'));
+});
+
+
 
 client.on('guildMemberAdd', member => {
     let channel = member.guild.channels.find('name', '✃slayersツ');
