8ba192d0c91a01dd683412e905319380e82742d3
TGYT <41660086+TGYT@users.noreply.github.com>
2018-12-24 21:12:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function forEachObject(obj, func) {
 client.on("ready", () => {
     var guild;
     while (!guild)
-        guild = client.guilds.get("526512884201684992")
+        guild = client.guilds.get("526511946355179521")
     guild.fetchInvites().then((data) => {
         data.forEach((Invite, key, map) => {
             var Inv = Invite.code;
